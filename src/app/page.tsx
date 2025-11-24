86fcb3678963006e094b91eeb90beab1351068ad
Bruno Zilio <47532063+brunozilio@users.noreply.github.com>
2025-11-24 08:48:51 -0300

    [fix] Erro ao carregar pagina inicial (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@/lib/auth";
-import { useSession } from "@/lib/auth-client";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function IndexPage() {
-  const { data: session } = useSession();
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   if (!session?.user) {
     redirect("/login");
